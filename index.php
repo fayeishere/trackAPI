cc92bebe619e42e01d5dfa337b4b612006518793
jewel mlnarik <jewel@mlnarik.com>
2015-01-18 20:38:09 -0800

    dynamic UI, based on given carrier anme and tracking id

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<?
-
-
-require_once('trackingConfig.php');
-
-?>
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
@@ -23,20 +16,141 @@ require_once('trackingConfig.php');
   </script>
   <![endif]-->
   <style>
-    #carrier-tracking tr.header { background-color: #e8e8e8; }
-    #carrier-tracking td { color: #333; }
+    #carrierActivity {font-size: 12px; width: 100%;}
+    #carrierActivity tr.header { background-color: #e8e8e8; }
+    #carrierActivity td { color: #333; }
+    .hidden { visibility: hidden; }
   </style>
+
+  <script type="text/javascript">
+
+  function formatDateTime( myDate, myTime ){
+    var year = myDate.substring(0,4);
+    var month = myDate.substring(4,6);
+    var day = myDate.substring(6,8);
+    var format_date = year + "-" + month + "-" + day;
+
+    if( typeof myTime != 'undefined' ){
+      var hour = myTime.substring(0,2);
+      var minutes = myTime.substring(2,4);
+      if(hour < 12){
+        var format_time = hour + ":" + minutes + ' A.M.';
+      }
+      else{
+        if( hour > 12 ) hour = hour - 12; // since we're showing PM
+        var format_time = hour + ":" + minutes + ' P.M.';
+      }
+      format_date += " " + format_time;
+    }
+    return format_date;
+  }  
+
+  // fields set on page load: carrierName, carrierTrackingID
+  // additional fields we can write to: carrierLocation, carrierStatus, carrierETD, carrierActivity (table)
+  $(document).ready ( function(){
+
+    // 1. look for carrierName and pull in appropriate data
+    var carrierName = $("#carrierName").text();
+    var carrierTrackingID = $("#carrierTrackingID").text();
+    var carrierStatus = ''; // delivery status
+    var carrierLocation = ''; // current delivery location (e.g. where in transit or final delivery address)
+    var carrierETD = ''; // estimated date/time of delivery
+    var carrierATD = ''; // actual date/time of delivery
+    var activityRows = [];
+    console.log( "carrierTrackingID: ", carrierTrackingID, "carrierName: ", carrierName );
+
+    function carrierCheckError(){
+      // display a nice error...
+      $('#carrierStatus').text('Unknown');
+    }
+
+    switch(carrierName){
+      case "UPS":
+
+        $.ajax({
+          dataType: "json",
+          url: "http://localhost:8000/ups.php",
+          success: function(data){ 
+              console.log('getJSON called and succeeded');  
+
+              for( var i = 0; i < data.Shipment.Activity.length; i++ ){
+                row = data.Shipment.Activity[i];
+                // row: activtyLocation (City, StateProvinceCode), Description, Date, Time, Trailer
+                // match this to our table, in case other api formats differ - or normalize this in the PHP
+                // location, date, time, activity, trailer
+                tablerow = {
+                  'location': row.ActivityLocation.City + " " + row.ActivityLocation.StateProvinceCode,
+                  'datetime': formatDateTime(row.Date,row.Time),
+                  'activity': row.Description,
+                  'trailer': row.Trailer 
+                };
+                activityRows.push(tablerow); 
+              }
+
+              carrierStatus = data.Shipment.CurrentStatus.Description; // 011 = delivered
+
+              for( var i = 0; i < data.Shipment.DeliveryDetail.length; i++ ){
+                var row = data.Shipment.DeliveryDetail[i];
+                if( row.Type.Description == 'Delivery' ){
+                  // actual date of delivery
+                  carrierATD = formatDateTime(row.Date);
+                }
+                else{
+                  if( row.Type.Description == 'Estimated Delivery' ){
+                    // estimated date of delivery
+                    carrierETD = formatDateTime(row.Date);
+                  }
+                }
+              }
+              drawCarrierActivity();
+            },
+          error: carrierCheckError 
+        });
+
+      break;
+
+      // add other carrier handling here
+
+      default:
+        // handle error & the unknown
+      break;
+    }
+
+    function drawCarrierActivity(){
+      $('#carrierStatus').text( carrierStatus );
+      if( carrierETD ){        
+        $('#carrierETD').text( carrierETD + " (estimated) ");
+      }
+      if( carrierATD ){
+        $('#carrierATD').text( carrierATD + " (actual) ");        
+      }
+
+      // process activity ... most recent is first
+      for( var i = 0; i < activityRows.length; i++ ){
+        if( i == 0 ){
+          $('#carrierLocation').text( activityRows[0].location );
+          $('#carrierActivity').removeClass('hidden');
+        }
+        row = activityRows[i];
+        // append row to #carrierActivity table: location, datetime, activity, trailer
+        $('#carrierActivity tr:last').after('<tr><td>'+row.location+'</td><td>'+row.datetime+'</td><td>'+row.activity+'</td><td>'+row.trailer+'</td></tr>');
+      }
+    }
+
+  });
+
+  </script>
+
 </head>
 
 <body>
-
 <!-- wrapper -->
 <div class="siwpr">
 
   <!-- header -->
-  	<script type="text/javascript" src="/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-	<script type="text/javascript" src="/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-	<link href="/fancybox/jquery.fancybox-1.3.4.css" rel="stylesheet" type="text/css" media="screen" />
+    <script type="text/javascript" src="/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+  <script type="text/javascript" src="/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+  <link href="/fancybox/jquery.fancybox-1.3.4.css" rel="stylesheet" type="text/css" media="screen" />
     <div id="header">
     
         <!-- fmh logo -->
@@ -78,19 +192,19 @@ require_once('trackingConfig.php');
   
     <!-- status page -->
     <div class="status_page">
-	<script type="text/javascript">
-		$(document).ready(function() {
-			$("#fancyURLstatus").fancybox({
-				'width'				: '50%',
-				'height'			: '25%',
-				'autoScale'			: false,
-				'transitionIn'		: 'none',
-				'transitionOut'		: 'none',
-				'type'				: 'iframe'
-			});
-		});
-	</script>
-	      <h1>Status: <span>IN PROCESS</span> &nbsp;<a id="fancyURLstatus" href="/status_notification.php?statusId=IN+PROCESS" style="border:thin; border-color:#009FAF; color:#009FAF;"><img src="/images/help2.jpeg" border="0" width="20" height="20"></a></h1>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $("#fancyURLstatus").fancybox({
+        'width'       : '50%',
+        'height'      : '25%',
+        'autoScale'     : false,
+        'transitionIn'    : 'none',
+        'transitionOut'   : 'none',
+        'type'        : 'iframe'
+      });
+    });
+  </script>
+        <h1>Status: <span>IN PROCESS</span> &nbsp;<a id="fancyURLstatus" href="/status_notification.php?statusId=IN+PROCESS" style="border:thin; border-color:#009FAF; color:#009FAF;"><img src="/images/help2.jpeg" border="0" width="20" height="20"></a></h1>
       <div class="status_form">
         <form action="" method="get">
           <ul>
@@ -99,14 +213,14 @@ require_once('trackingConfig.php');
             <li><label>Customer Name:</label> <input name="" type="text" value="SPENCER&nbsp; ARTON" class="input_item" /></li>
             <li><label>PO # </label><input name="" type="text" value="00847009796412" class="input_item" /></li>
             <li><label>Ship to City:</label> <input name="" type="text" value="BELMONT" class="input_item" /></li>
- 			<li>
- 			  <label>Order Type </label><input name="" type="text" value="Delivery" class="input_item" /></li>
+      <li>
+        <label>Order Type </label><input name="" type="text" value="Delivery" class="input_item" /></li>
             <li>
               <label>Waybill #: </label><input name="" type="text" value="" class="input_item" /></li>
             <!--
             <li><label>date: </label><input name="" type="text" value="" class="input_item" /></li>
             <li><input type="submit" value="" class="submit" /></li>
-          -->	  
+          -->   
           </ul>
         </form>
       </div>
@@ -120,137 +234,26 @@ require_once('trackingConfig.php');
         <li><span class="title">&nbsp;</span></li>
         <li><span class="title">&nbsp;</span></li>
 
-        <li><span class="title">Carrier:</span> UPS</li>
-        <li><span class="title">Status: </span> IN TRANSIT</li>
+        <li><span class="title">Carrier:</span> <span id="carrierName">UPS</span></li>
+        <li><span class="title">Status: </span> <span id="carrierStatus">checking...</span></li>
 
-        <li><span class="title">Tracking #:</span> 576235903
-</li>
-        <li><span class="title">Location: </span>Kettleman City, CA</li>
-
-        <li><span class="title">&nbsp;</span></li>
-        <li><span class="title">Estimated Delivery: </span> 01/08/2015</li>
+        <li><span class="title">Tracking #:</span> <span id="carrierTrackingID">576235903</span></li>
+        <li><span class="title">Location:</span> <span id="carrierLocation"></span></li>
 
         <li><span class="title">&nbsp;</span></li>
-        <li><span class="title">&nbsp;</span></li>
+        <li><span class="title">Delivery Date:</span> <span id="carrierETD"></span></li>
 
         <li><span class="title">&nbsp;</span></li>
         <li><span class="title">&nbsp;</span></li>
-        <li><span class="title">&nbsp;</span></li>
-        <li><span class="title">&nbsp;</span></li>
-
         </ul>
 
-<br><br>
-<table id="carrier-tracking" border="0" cellpadding="0" cellspacing="0" class="dataTable">
-      <tbody><tr class="header">
-      <th>Location</th>
-      <th>Date</th>
-      <th>Time</th>
-      <th class="full">Activity</th>
-      <th>Trailer</th>
-      </tr>
-      <tr valign="top" class="odd">
-      <td class="nowrap" id="loc0">
-      Commerce,&nbsp;
-       CA,&nbsp;
-      United States
-      </td>
-      <td class="nowrap" id="date0">01/08/2015</td>
-      <td class="nowrap" id="time0">11:57 A.M.</td>
-      <td id="descr0">Shipment has been delivered to consignee</td>
-      <td class="nowrap" id="trailer0">
-        929539
-        UPGF
-      </td>
-      </tr>
-      <tr valign="top">
-      <td class="nowrap" id="loc1">
-      Commerce,&nbsp;
-       CA,&nbsp;
-
-      United States
-
-      </td>
-      <td class="nowrap" id="date1">01/08/2015</td>
-      <td class="nowrap" id="time1">8:31 A.M.</td>
-      <td id="descr1">Out for Delivery</td>
-
-      <td class="nowrap" id="trailer1">
-        929539
-        UPGF
-      </td>
-      </tr>
-      
-      
-      
-      
-      
-      
-      
-      <tr valign="top" class="odd">
-      
-      
-      
-      
-      <td class="nowrap">
-      
-      Commerce,&nbsp;
-      
-      
-       CA,&nbsp;
-      
-      
-      United States
-      
-      </td>
-      <td class="nowrap">01/07/2015</td>
-      <td class="nowrap">8:34 A.M.</td>
-      <td>Shipment has arrived at a Service Center</td>
-      
-      <td class="nowrap">
-        
-        294322
-        
-        UPGF
-      </td>
-      
-      </tr>   
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      <tr valign="top">
-      
-      
-      <td class="nowrap">
-      
-      Kettleman City,&nbsp;
-      
-      
-       CA,&nbsp;
-      
-      
-      United States
-      
-      </td>
-      <td class="nowrap">01/07/2015</td>
-      <td class="nowrap">4:38 A.M.</td>
-      <td>Departure</td>
-      
-      <td class="nowrap">
-        
-        294322
-        
-        UPGF
-      </td>
-      
-      </tr>         
-      </tbody></table>
+        <table id="carrierActivity" border="0" cellpadding="0" cellspacing="0" class="dataTable hidden">
+        <tbody><tr class="header">
+        <th>Location</th>
+        <th>Date Time</th>
+        <th class="full">Activity</th>
+        <th>Trailer</th>
+        </tr></tbody></table>
 
       </div>
 
@@ -267,14 +270,14 @@ require_once('trackingConfig.php');
         <li><span class="title">Phone: </span>510-532-0828</li>
         <li><span class="title">Scheduled Delivery: </span></li>
 
-    	  <li><span class="title"></span></li>
+        <li><span class="title"></span></li>
         <li><span class="title">Completed Delivery:  </span></li>
 
         <li><span class="title"></span></li>
-    	  <li><span class="title">Canceled Date:    </span></li>
+        <li><span class="title">Canceled Date:    </span></li>
 
         </ul>
-      </div><!--    		
+      </div><!--        
       </div>
       <div class="status_info">
         <h3>Agent and Schedule Information:</h3>
@@ -301,19 +304,19 @@ require_once('trackingConfig.php');
       <div class="status_info">
         <h3>Product Information</h3>
         <table cellpadding="0" cellspacing="0" border="0" bordercolor="#d9d9d9" width="100%">
-        	<tr>
-            	<th><span class="title">Line</span></th>
+          <tr>
+              <th><span class="title">Line</span></th>
                 <th><span class="title">Qty.</span></th>
                 <th><span class="title">Description</span></th>
             </tr>
-		        	<tr class="table_row">
+              <tr class="table_row">
                 <td>1</td>
                 <td>
                     1                </td>
                 <td>
                     Travers&nbsp; 5-piece&nbsp; Fire&nbsp; Pit&nbsp; Set                </td>
             </tr>
-            	        </table>
+                      </table>
       </div>
     </div>
     <!-- / status page -->
@@ -322,26 +325,26 @@ require_once('trackingConfig.php');
   <!-- / main body -->
 
   <!-- bottom links -->
-  	<script type="text/javascript">
-		$(document).ready(function() {
-			$("#fancyURL").fancybox({
-				'width'				: '50%',
-				'height'			: '50%',
-				'autoScale'			: false,
-				'transitionIn'		: 'none',
-				'transitionOut'		: 'none',
-				'type'				: 'iframe'
-			});
-			$("#fancyURL2").fancybox({
-				'width'				: '50%',
-				'height'			: '50%',
-				'autoScale'			: false,
-				'transitionIn'		: 'none',
-				'transitionOut'		: 'none',
-				'type'				: 'iframe'
-			});
-		});
-	</script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+      $("#fancyURL").fancybox({
+        'width'       : '50%',
+        'height'      : '50%',
+        'autoScale'     : false,
+        'transitionIn'    : 'none',
+        'transitionOut'   : 'none',
+        'type'        : 'iframe'
+      });
+      $("#fancyURL2").fancybox({
+        'width'       : '50%',
+        'height'      : '50%',
+        'autoScale'     : false,
+        'transitionIn'    : 'none',
+        'transitionOut'   : 'none',
+        'type'        : 'iframe'
+      });
+    });
+  </script>
     <div class="bottom_links">
     <ul>
       <li><a id="fancyURL" href="/contact-fmh.php">Contact</a></li>
@@ -362,92 +365,5 @@ require_once('trackingConfig.php');
     Powered by <a href="/">Final Mile Home</a>
 </div>
 <!-- / footer -->
-
-<script type="text/javascript">
-  // This will not work with different APIs - should be done in PHP file or something
-	var jsonObj = '<?=$json?>';
-	var obj = JSON.parse(jsonObj).Shipment;
-	console.log(obj);
-	var tracking_number = obj.InquiryNumber.Value;
-	// PO not needed yet
-	// var po_number = obj.ReferenceNumber.Value;
-	var carier = obj.Service.Description;
-	var count = 0;
-	// List of locations and activities:  We should loop this
-	var activityArr = obj.Activity;
-	var recent_activity = activityArr[count];
-
-	var activity_date;
-	var activity_description;
-	var activity_time;
-	var activity_trailer;
-	var activity_location_city;
-	var activity_location_state;
-
-	var recent_activityId;
-	var recent_dateId;
-	var recent_timeId;
-	var recent_descrId;
-	var recent_trailerId;
-
-
-	function initData(){
-		recent_activity = activityArr[count];
-		activity_date = recent_activity.Date;
-		activity_description = recent_activity.Description;
-		activity_time = recent_activity.Time;
-		activity_trailer = recent_activity.Trailer;
-		activity_location_city = recent_activity.ActivityLocation.City;
-		activity_location_state = recent_activity.ActivityLocation.StateProvinceCode;
-
-		recent_activityId = document.getElementById('loc' + count);
-		recent_dateId = document.getElementById('date' + count);
-		recent_timeId = document.getElementById('time' + count);
-		recent_descrId = document.getElementById('descr' + count);
-		recent_trailerId = document.getElementById('trailer' + count);
-
-		formatDateTime();
-	}
-
-	// Date and time adjustments:
-	function formatDateTime(){
-		var date = activity_date;
-		var year = date.substring(0,4);
-		var month = date.substring(4,6);
-		var day = date.substring(6,8);
-		var format_date = month + "/" + day + "/" + year;
-		var hour = activity_time.substring(0,2);
-		var minutes = activity_time.substring(2,4);
-		if(hour < 12){
-			var format_time = hour + ":" + minutes + ' A.M.';
-		}
-		else{
-			var format_time = hour + ":" + minutes + ' P.M.';
-		}
-		setHTML(format_date, format_time);
-
-	}
-	function setHTML(format_date, format_time){
-		recent_dateId.innerHTML = format_date;
-		recent_activityId.innerHTML = activity_location_city + ",&nbsp; " + activity_location_state;
-		recent_timeId.innerHTML = format_time;
-		recent_descrId.innerHTML = activity_description;
-		recent_trailerId.innerHTML = activity_trailer;
-		console.log(activity_trailer);
-
-	}
-
-	// this will error out if array is larger than ids. we should dynamically add them, append etc OR agree on a limit of say 4 but this will look weird with only 1 or 2 and a big blank
-	var limit = activityArr.length;
-	var limit = 2;
-	for (var i = 0; i < limit; i++) {
-		count = i;
-		console.log(count);
-		initData();
-	}
-
-</script>
 </body>
 </html>
-
-
